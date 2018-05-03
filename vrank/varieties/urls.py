547eedca9a25ee93dff5295e75f603ba40466b30
originalhumanbeing <han.hyunjung17@gmail.com>
2018-05-04 01:09:34 +0900

    Delete UserViewSet

--- vrank/varieties/urls.py
+++ vrank/varieties/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url, include
-from .views import VarietyShowViewSet, UserViewSet
+from .views import VarietyShowViewSet
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
 router.register(r'all', VarietyShowViewSet)
-router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
