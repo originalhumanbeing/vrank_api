27c843cdee9770159ddf7b48eb66bf57cd978021
originalhumanbeing <han.hyunjung17@gmail.com>
2018-05-04 01:08:40 +0900

    Delete UserViewSet

--- vrank/varieties/views.py
+++ vrank/varieties/views.py
@@ -1,16 +1,7 @@
-from django.contrib.auth.models import User
 from rest_framework import viewsets
 
 from .models import VarietyShow
-from .serializers import UserSerializer, VarietyShowSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+from .serializers import VarietyShowSerializer
 
 
 class VarietyShowViewSet(viewsets.ModelViewSet):
